24757fbb0182fbcaabded47e73ffb87e0eb48044
putriamalia18 <putriamalia180806@gmail.com>
2026-02-13 06:03:56 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,8 @@ function toggle(id) {
     const content = document.getElementById(id);
     content.classList.toggle("show");
 
-    // Pause & reset audio/video jika konten disembunyikan
-    const media = content.querySelector('audio, video');
-    if(media && !content.classList.contains("show")) {
+    const media = content.querySelector("audio, video");
+    if (media && !content.classList.contains("show")) {
         media.pause();
         media.currentTime = 0;
     }
